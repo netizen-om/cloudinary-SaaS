81aea5b32f604d955c905cfe264de6ed2810c539
netizen-om <borisagarom@gmail.com>
2025-02-01 11:28:45 +0530

    clerk and daisyui setup complete

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import { auth, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextRequest, NextResponse } from 'next/server';
+
+const isPublicRoute = createRouteMatcher([
+    "/signup",
+    "/signin",
+    "/",
+    "/home",
+])
+
+const isPublicApiRoute = createRouteMatcher([
+    "/api/videos"
+])
+
+export default clerkMiddleware(async(auth, req) => {
+    const {userId} = await auth()
+    const currentUrl = new URL(req.url)
+    const isAccessingDashboard = currentUrl.pathname === "/home"
+    const isApiRequest = currentUrl.pathname.startsWith("/api")
+
+    if(userId && isPublicRoute(req) && !isAccessingDashboard){
+        return NextResponse.redirect(new URL("/home", req.url))
+    }
+    //if not logged in
+    if(!userId){
+        if(!isPublicRoute(req) && !isPublicApiRoute(req)){
+            return NextResponse.redirect(new URL("/signin", req.url))
+        }
+        
+        if(isApiRequest && !isPublicApiRoute(req)){
+            return NextResponse.redirect(new URL("/signin", req.url))
+        }
+    }
+
+    return NextResponse.next()
+})
+
+export const config = {
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+}
