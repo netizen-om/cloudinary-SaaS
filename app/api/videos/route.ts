8432cc152d49235753a36ca0f38a6d032d55f6b7
netizen-om <borisagarom@gmail.com>
2025-02-01 17:10:50 +0530

    prisma and neonDB setup complete

---
+++ app/api/videos/route.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient()
+
+export async function GET(request : NextRequest){
+    try {
+        const video = await prisma.video.findMany({
+            orderBy : {createdAt : "desc"}
+        })
+
+        return NextResponse.json(video)
+    } catch (error) {
+        return NextResponse.json(
+            {error : "Error fetching videos"},
+            {status : 500}
+        )
+    } finally {
+        await prisma.$disconnect()
+    }
+}
