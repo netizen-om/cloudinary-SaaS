609a2413a1b2bd691cda243cee36825206a89c40
netizen-om <borisagarom@gmail.com>
2025-02-03 18:55:21 +0530

    functional part complete

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,8 +1,72 @@
-import React from 'react'
+"use client"
+import VideoCard from '@/components/VideoCard'
+import axios from 'axios'
+import { Video } from "@/types/index"
+import React, { useCallback, useEffect, useState } from 'react'
 
 function Home() {
+
+  const [videos, setVideos] = useState<Video[]>([])
+  const [loading,setLoading] = useState(true)
+  const [error, SetError] = useState(null)
+
+  const fetchVideos = useCallback(async() => {
+    try {
+      const response = await axios.get("/api/videos")
+      if(Array.isArray(response.data)){
+        setVideos(response.data)
+      } else {
+        throw new Error("Unexpected response format")
+      }
+    } catch (error : any) {
+      console.log(error);
+      SetError(error)
+    } finally {
+      setLoading(false)
+    }
+  },[])
+
+    useEffect(() => {
+      fetchVideos()
+    }, [fetchVideos])
+
+    const handleDownload = useCallback((url : string, title : string) => {
+        () => {
+              const link = document.createElement("a");
+              link.href = url;
+              link.setAttribute("download", `${title}.mp4`)
+              link.setAttribute("target", `_blank`)
+              document.body.appendChild(link);
+              link.click();
+              document.body.removeChild(link);
+        } 
+    }, [])
+
+    if(loading){
+      return <div>Loading..........</div>
+    }
+
   return (
-    <div>Home</div>
+    <div className="container mx-auto p-4">
+    <h1 className="text-2xl font-bold mb-4">Videos</h1>
+    {videos.length === 0 ? (
+      <div className="text-center text-lg text-gray-500">
+        No videos available
+      </div>
+    ) : (
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {
+          videos.map((video) => (
+            <VideoCard 
+              key={video.id}
+              video={video}
+              onDownLoad={handleDownload}
+            />
+          ))
+        }
+      </div>
+    )}
+  </div>
   )
 }
 
---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export interface Video {
+    id : string
+    title : string
+    description : string
+    originalSize : string
+    compressedSize : string
+    publicId : string
+    createdAt : Date
+    duration : number
+    updatedAt : Date
+}
