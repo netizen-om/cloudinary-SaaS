8cced1a1d43ed659ef44318f77c9f9cfe066a016
netizen-om <borisagarom@gmail.com>
2025-02-05 19:21:47 +0530

    sign-in page layout fix

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  // return <SignIn />
+  return (
+    <div className='min-h-screen bg-[#1D232A] w-full flex justify-center items-center'>
+      <SignIn />
+    </div>
+  )
 }
