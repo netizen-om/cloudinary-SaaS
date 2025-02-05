f8e497c917852c866056b27b9a02c0981ab45a5f
netizen-om <borisagarom@gmail.com>
2025-02-05 19:22:24 +0530

    sign-up page layout fix

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return (
+      <div className='min-h-screen bg-[#1D232A] w-full flex justify-center items-center'>
+        <SignUp />
+      </div>
+    )
 }
